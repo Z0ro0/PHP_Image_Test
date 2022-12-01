608c2485fc609afd07b2afbed547b63a592bb180
Z0ro0 <chlrkdmf0805@gmail.com>
2022-12-01 10:02:42 +0900

    파일 업로드 하면 abc 만들기

--- upload_result.php
+++ upload_result.php
@@ -8,5 +8,12 @@ $f_size=$_FILES['userfile']['size'];
 
 $f_tmpname=$_FILES['userfile']['tmp_name'];
 
+echo "파일이름: ".$f_name."<br/>";
+echo "파일타입: ".$f_type."<br/>";
+echo "파일임시이름: ".$f_tmpname."<br/>";
 
+$upload_dir='./abc/';
+if(!is_dir($upload_dir)){
+    mkdir($upload_dir);
+}
 ?>
