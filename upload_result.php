75fe30cb73b8cc517c9d898a6c900aaa21f3305b
Z0ro0 <chlrkdmf0805@gmail.com>
2022-12-08 09:03:47 +0900

    db에 업로드한 파일 추가되게

--- upload_result.php
+++ upload_result.php
@@ -1,27 +1,27 @@
 <?php
+ include ('./db_conn.php');
+ $f_name=$_FILES['userfile']['name'];
+ $f_type=$_FILES['userfile']['type'];
+ $f_size=$_FILES['userfile']['size'];
+ $f_tmpname=$_FILES['userfile']['tmp_name'];
 
-$f_name=$_FILES['userfile']['name'];
-
-$f_type=$_FILES['userfile']['type'];
-
-$f_size=$_FILES['userfile']['size'];
-
-$f_tmpname=$_FILES['userfile']['tmp_name'];
-
-echo "파일이름: ".$f_name."<br/>";
-echo "파일타입: ".$f_type."<br/>";
-echo "파일임시이름: ".$f_tmpname."<br/>";
-
-$upload_dir='./abc/';
-if(!is_dir($upload_dir)){
+ $upload_dir='./abc/';
+ if(!is_dir($upload_dir)) {
     mkdir($upload_dir);
-}
+ }
+ //To do  진짜 파일이름
+ $upload_file1=$upload_dir.basename($f_name);
+ if(move_uploaded_file($f_tmpname,$upload_file1)) {
+    echo "업로드 되었습니다";
 
-$upload_file1=$upload_dir.basename($f_name);
+ }
+ else {
+    "업로드 실패";
+ }
+  
+//insert 쿼리  img_path :$upload_file1
+$sql="insert into img(img_name, img_type, img_size, img_path) values('f_name','f_type',$f_size,'$upload_file1')";
+echo "추가되었습니다";
+mysqli_query($conn,$sql);
 
-if(move_uploaded_file($f_tmpname, $upload_file1)){
-    echo"업로드 되었습니다";
-}else{
-    echo"업로드 실패";
-}
 ?>
